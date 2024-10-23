e53f37036a77f4c1fbbf56010c396aa5b5710265
jjoacoc <jjoaco.cofre@gmail.com>
2024-10-23 00:54:06 -0300

    Api

---
+++ controllers/UserController.php
@@ -0,0 +1,77 @@
+<?php
+// app/controllers/ProductController.php
+
+include_once '../models/User.php';
+include_once '../core/Database.php';
+
+class UserController {
+    private $db;
+    private $user;
+
+    public function __construct() {
+        $database = new Database();
+        $this->db = $database->getConnection();
+        $this->user = new User($this->db);
+    }
+
+    // Obtener todos los Usuarios
+    public function getAll() {
+        $stmt = $this->user->getAll();
+        $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return json_encode($user);
+    }
+
+    // Obtener un Usuario por ID
+    public function getById($Id_Usuarios) {
+        $user = $this->user->getById($Id_Usuarios);
+        return json_encode($user);
+    }
+
+    // Crear un nuevo Usuario
+    public function create($data) {
+        $this->user->Nombres = $data->Nombres;
+        $this->user->email = $data->email;
+        $this->user->Pass = $data->Pass;
+        $this->user->Id_Usuarios = $data->Id_Usuarios;
+        if ($this->user->create()) {
+            return json_encode(["message" => "OK"]);
+        }
+        return json_encode(["message" => "Error al crear el Usuario"]);
+    }
+
+    // Actualizar un Usuario
+    public function update($Id_Usuarios, $data) {
+        $this->user->Nombres = $data->Nombres;
+        $this->user->email = $data->email;
+        $this->user->Pass = $data->Pass;
+        $this->user->Id_Usuarios = $data->Id_Usuarios;
+        if ($this->user->update($Id_Usuarios)) {
+            return json_encode(["message" => "OK"]);
+        }
+        return json_encode(["message" => "Error al actualizar el Usuario"]);
+    }
+
+    // Eliminar un Usuario
+    public function delete($Id_Usuarios) {
+        if ($this->user->delete($Id_Usuarios)) {
+            return json_encode(["message" => "OK"]);
+        }
+        return json_encode(["message" => "Error al eliminar el Usuario"]);
+    }
+
+
+     // Autenticar usuario
+     public function login($data)
+     {
+         $email = $data->email;
+         $Pass = $data->Pass;
+         $user = $this->user->authenticate($email, $Pass);
+         if ($user) {
+             return json_encode(["resultado" => "OK", "usuario" => $user]);
+         } else {
+             return json_encode(["resultado" => "Error", "mensaje" => "Credenciales incorrectas"]);
+         }
+     }
+
+}
+?>
---
+++ core/Database.php
@@ -0,0 +1,24 @@
+<?php
+// app/core/Database.php
+
+class Database {
+    private $host = "172.16.20.30";
+    private $db_name = "Investigacion_miercoles";
+    private $username = "desarrollo";
+    private $password = "fisca1234";
+    private $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Error de conexión: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
