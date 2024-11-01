a3c85bb6ccb55b70fecda6c51355f539ebd70bc9
Sba188 <Sebad2607@gmail.com>
2024-11-01 02:29:05 -0300

    por si me quedo dormido,aca esta la tabal de areas echa sin ts

--- src/app/modules/configuracion/configuracion.module.ts
+++ src/app/modules/configuracion/configuracion.module.ts
@@ -4,6 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ConfiguracionRoutingModule } from './configuracion-routing.module';
 import { AreaDeServicioComponent } from './area-de-servicio/area-de-servicio.component';
 import { TipoDeIncidentesComponent } from './tipo-de-incidentes/tipo-de-incidentes.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 
 @NgModule({
@@ -13,7 +19,13 @@ import { TipoDeIncidentesComponent } from './tipo-de-incidentes/tipo-de-incident
   ],
   imports: [
     CommonModule,
-    ConfiguracionRoutingModule
+    ConfiguracionRoutingModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    MatCheckboxModule
   ]
 })
 export class ConfiguracionModule { }
--- src/app/modules/configuracion/tipo-de-incidentes/tipo-de-incidentes.component.ts
+++ src/app/modules/configuracion/tipo-de-incidentes/tipo-de-incidentes.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./tipo-de-incidentes.component.css']
 })
 export class TipoDeIncidentesComponent {
+  displayedColumns: string[] = ['select', 'areaDeServicio', 'tiposDeIncidentes', 'usuarios'];
+  
+
+  filtroGeneral: string = '';
+  filtroUsuarios: string = '';
+  cargoSeleccionado: string = 'Todos';
+  habilitacionSeleccionada: string = 'Habilitados';
+
+  // Datos de ejemplo para áreas de servicio
+
+
+ 
+
+  cargos: string[] = ['Todos', 'Admin', 'Usuario', 'Operador'];  // Ejemplo de cargos
+
+
+ 
+
+
+
+  // Filtro de usuarios (por ahora vacío)
+  filtrarUsuarios() {
+    // Lógica para filtrar usuarios
+  }
+
 
 }
+
